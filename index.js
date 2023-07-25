0a33f49cb3b1d0632d7b2b670dba97e7b5fdbb41
Molly Todd <mollyjt89@gmail.com>
2023-07-25 16:10:13 -0700

    COMPLETED LAB

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Global variables
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob';
+
+//functions
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase()
+return customerName
+}
+
+console.log(upperCaseCustomerName(bob));
+
+function setBestCustomer() {
+bestCustomer = 'not bob'; 
+return bestCustomer
+}
+console.log(setBestCustomer());
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob' ;
+    return bestCustomer
+}
+console.log(overwriteBestCustomer());
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'molly';
+}
